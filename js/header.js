05d15219bcd89f370ee5d5760645149e9a1034f5
Antony Martinez Ruiz <antonymaruiz02@gmail.com>
2024-02-01 22:38:33 -0500

    Hola

---
+++ js/header.js
@@ -0,0 +1,109 @@
+/* <section class="signIn_Form">
+    <form class="formSing" id="form">
+        <div class="signIn_Subtitle">
+            <p>Username</p>
+        </div>
+        <div class="signIn_Form_Option">
+            <label for="Username">
+                <img
+                    src="/media/icons/user input.svg"
+                    alt="user_input"
+                    class="signIn_Form_User_Icon"
+                />
+            </label>
+            <input
+                id="Username"
+                class="signIn_Form_Username"
+                type="text"
+                placeholder="Username"
+                required
+            />
+        </div>
+        <div class="signIn_Subtitle">
+            <p>Password</p>
+        </div>
+        <div class="signIn_Form_Option">
+            <label for="password">
+                <img
+                    id="Key_Icon"
+                    src="/media/icons/key input.svg"
+                    alt="key_input"
+                    class="signIn_Form_Key_Icon"
+                />
+            </label>
+            <input
+                id="password"
+                class="signIn_Form_password"
+                type="password"
+                placeholder="**********"
+                required
+            />
+            <img
+                id="Eye_Icon"
+                src="/media/icons/eye input.svg"
+                alt="eye_input"
+                class="signIn_Form_Eye_Icon"
+            />
+            <img
+                id="Eye_Icon_Show"
+                src="/media/icons/ojo.png"
+                alt="eye_input"
+                class="signIn_Form_Eye_Icon"
+            />
+        </div>
+
+        <div class="signIn_Checkbox">
+            <div>
+                <input
+                    class="signIn_Checkbox_Remember"
+                    type="checkbox"
+                    id="remember"
+                />
+                <label for="remember">Remember me</label>
+            </div>
+            <a href="#">Forgot password?</a>
+        </div>
+
+        <button class="signIn_Button" type="submit">SIGN IN</button>
+    </form>
+
+    <div class="SignIn_Continue">
+        <div class="SignIn_Continue"></div>
+        <p>Or continue with</p>
+    </div>
+
+    <div class="SignIn_SocialMedia">
+        <!-- reviar los href -->
+        <div class="SocialMedia_Google">
+            <a href="https://www.gmail.com">
+                <img
+                    class="signIn_SocialMedia_Google"
+                    src="/media/icons/google.svg"
+                    alt="Aconunt Google"
+                />
+            </a>
+        </div>
+        <div class="SocialMedia_Whatsapp">
+            <a href="https://web.whatsapp.com">
+                <img
+                    class="signIn_SocialMedia_Whatsapp"
+                    src="/media/icons/whatsapp.svg"
+                    alt="Aconunt whatsapp"
+                />
+            </a>
+        </div>
+        <div class="SocialMedia_Facebook">
+            <a href="https://facebook.com">
+                <img
+                    class="signIn_SocialMedia_Facebook"
+                    src="/media/icons/facebook.svg"
+                    alt="Aconunt facebook"
+                />
+            </a>
+        </div>
+    </div>
+    <div class="signIn_CreateAccount">
+        <label for="don't have">Don't have an account?</label>
+        <a href="/pages/signUp">Sign up</a>
+    </div>
+</section> */
