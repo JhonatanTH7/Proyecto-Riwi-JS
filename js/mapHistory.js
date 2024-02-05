52d9e31caa0e3c997fbd3faac47ede8dc3082434
JhonatanTH7 <jhonatantorohurtado93@gmail.com>
2024-02-05 00:52:11 -0500

    actualizacion history card terminada

--- js/mapHistory.js
+++ js/mapHistory.js
@@ -2,4 +2,10 @@
 const star = document.querySelector(".star");
 
 // Eventos
-star.addEventListener("")
+star.addEventListener("click", () => {
+  if (star.getAttribute("src") == "../../media/icons/star gray.svg") {
+    star.setAttribute("src", "../../media/icons/star yellow.svg");
+  } else {
+    star.setAttribute("src", "../../media/icons/star gray.svg");
+  }
+});
