ea20cab9fa1b4b5d3192fa45cc21b4a196450fe7
JhonatanTH7 <jhonatantorohurtado93@gmail.com>
2024-02-08 02:42:47 -0500

    updateroades

--- js/updateRoadStatus.js
+++ js/updateRoadStatus.js
@@ -1,7 +1,43 @@
-const arsd = document.querySelector(".menu-left")
+const arsd = document.querySelector(".menu-left");
 
-document.addEventListener("click", ()=>{
-    setTimeout(() => {
-        console.log(arsd.getBoundingClientRect());
-    }, 1000);
-})
+document.addEventListener("click", () => {
+  setTimeout(() => {
+    console.log(arsd.getBoundingClientRect());
+  }, 1000);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  getData();
+});
+
+async function getData() {
+  const respuesta = await fetch("http://localhost:3000/notification");
+  const data = await respuesta.json();
+  drawTable(data);
+}
+
+function drawTable(notifications) {
+  const table = document.querySelector(".update_Table_Notifications");
+  let id;
+  notifications.forEach((notification) => {
+    id = "alertNotification";
+    if (notification.type == "caution") {
+      id = "cautionNotification";
+    }
+    if (notification.type == "info") {
+      id = "infoNotification";
+    }
+    if (notification.type == "news") {
+      id = "newsNotification";
+    }
+    table.innerHTML += `
+    <tr>
+    <td colspan="4">
+      <div class="collapse" id="${id}">
+        <div class="card card-body">${notification.content}</div>
+      </div>
+    </td>
+  </tr>
+    `;
+  });
+}
